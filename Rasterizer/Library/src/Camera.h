e16ec551aa348c783d1e419aa1321f80336843fa
RamsesEveraert <54176877+RamsesEveraert@users.noreply.github.com>
2023-11-16 11:51:58 +0100

    Added Camera Update functions

--- Rasterizer/Library/src/Camera.h
+++ Rasterizer/Library/src/Camera.h
@@ -30,6 +30,10 @@ namespace dae
 		float totalPitch{};
 		float totalYaw{};
 
+		float rotationSpeed{ 0.01f };
+		float moveSpeed{ 10.f };
+		float dragSpeed{ 5.0f };
+
 		Matrix invViewMatrix{};
 		Matrix viewMatrix{};
 
@@ -61,14 +65,64 @@ namespace dae
 
 		void Update(Timer* pTimer)
 		{
-			const float deltaTime = pTimer->GetElapsed();
+			
 
 			//Camera Update Logic
-			//...
+			HandleInputs(pTimer);
 
 			//Update Matrices
 			CalculateViewMatrix();
 			CalculateProjectionMatrix(); //Try to optimize this - should only be called once or when fov/aspectRatio changes
+
 		}
+
+		void HandleInputs(Timer* pTimer)
+		{
+			const float dt = pTimer->GetElapsed();
+			HandleKeyboardInput(dt);
+			HandleMouseInput(dt);
+		}
+
+		void HandleKeyboardInput(float dt)
+		{
+			const uint8_t* pKeyboardState = SDL_GetKeyboardState(nullptr);
+
+			int8_t zDirection{ pKeyboardState[SDL_SCANCODE_W] - pKeyboardState[SDL_SCANCODE_S] };
+			int8_t xDirection{ pKeyboardState[SDL_SCANCODE_D] - pKeyboardState[SDL_SCANCODE_A] };
+			int8_t yDirection{ pKeyboardState[SDL_SCANCODE_E] - pKeyboardState[SDL_SCANCODE_Q] };
+
+			origin += zDirection * moveSpeed * forward * dt;
+			origin += xDirection * moveSpeed * right * dt;
+			origin += yDirection * moveSpeed * up * dt;
+		}
+
+		void HandleMouseInput(float dt)
+		{
+			int mouseX, mouseY;
+			const uint32_t mouseState = SDL_GetRelativeMouseState(&mouseX, &mouseY);
+
+			//Mouse buttons
+			bool isLeftMousePressed{ static_cast<bool>(mouseState & SDL_BUTTON(1)) && !static_cast<bool>(mouseState & SDL_BUTTON(3)) }; // prevent double mouse button click
+			bool isRightMousePressed{ static_cast<bool>(mouseState & SDL_BUTTON(3)) && !static_cast<bool>(mouseState & SDL_BUTTON(1)) }; // Prevent double mouse button click
+			bool areBothMouseButtonsPressed{ static_cast<bool>(mouseState & SDL_BUTTON(1)) && static_cast<bool>(mouseState & SDL_BUTTON(3)) };
+
+			SDL_SetRelativeMouseMode(static_cast<SDL_bool>(isLeftMousePressed || isRightMousePressed)); // https://metacpan.org/pod/SDL2::mouse
+
+			if (areBothMouseButtonsPressed)
+			{
+				origin -= mouseY * dragSpeed * Vector3::UnitY * dt;
+			}
+			else if (isRightMousePressed)
+			{
+				totalYaw += mouseX * rotationSpeed;
+				totalPitch -= mouseY * rotationSpeed;
+			}
+			else if (isLeftMousePressed)
+			{
+				origin -= mouseY * dragSpeed * forward * dt;
+				totalYaw += mouseX * rotationSpeed;
+			}
+		}
+
 	};
 }
